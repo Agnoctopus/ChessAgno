37c114e039483909314f4270bef46d0ec5fdc43b
César Belley <cesar.belley@lse.epita.fr>
2022-04-24 11:35:10 +0200

    Add piece type
    Signed-off-by: César Belley <cesar.belley@lse.epita.fr>

--- src/board/piece.rs
+++ src/board/piece.rs
@@ -1,6 +1,70 @@
+//! Pieces
 
+use std::slice::Iter;
+
+use super::color::Color;
+
+/// Number of pieces
 pub const PIECES_NUM: usize = 13;
+/// Number of color pieces
 pub const PIECES_COLOR_NUM: usize = 6;
+
+
+/// Piece type of every possible piece present on the board.
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[repr(u8)]
+pub enum PieceType {
+    King,
+    Queen,
+    Rook,
+    Bishop,
+    Knight,
+    Pawn
+}
+
+/// Piece types char representation
+const CHAR_PIECE_TYPES: [char; 6] = [
+    'K', 'Q', 'R', 'B', 'N', 'P'
+];
+
+
+impl PieceType {
+    /// All possible piece types, usefull for iteratation
+    const PIECE_TYPES: [PieceType; 6] = [
+        PieceType::King, PieceType::Queen, PieceType::Rook,
+        PieceType::Bishop, PieceType::Knight, PieceType::Pawn,
+    ];
+
+    /// Returns an iterator over all possible pieces types
+    #[inline]
+    pub fn all() -> Iter<'static, PieceType> {
+        Self::PIECE_TYPES.iter()
+    }
+
+    /// Returns the piece type char representation
+    #[inline]
+    pub fn to_char(self) -> char {
+        CHAR_PIECE_TYPES[self as usize]
+    }
+}
+
+impl TryFrom<char> for PieceType {
+    type Error = String;
+
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        let piece_type = match c {
+            'K' => PieceType::King,
+            'Q' => PieceType::Queen,
+            'R' => PieceType::Rook,
+            'B' => PieceType::Bishop,
+            'N' => PieceType::Knight,
+            'P' => PieceType::Pawn,
+            _ => return Err(format!("Unknown piece type: {}", c))
+        };
+
+        Ok(piece_type)
+    }
+}
 
 /// Piece representation
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
@@ -22,25 +86,35 @@ pub enum Piece {
 }
 
 /// Sliding pieces
-pub const SLIDING_PIECES: [bool; PIECES_NUM] = [
+const SLIDING_PIECES: [bool; PIECES_NUM] = [
     false,
     false, true, true, true, false, false,
     false, true, true, true, false, false
 ];
 
 /// Sliding diagonal pieces
-pub const SLIDING_DIAG_PIECES: [bool; PIECES_NUM] = [
+const SLIDING_DIAG_PIECES: [bool; PIECES_NUM] = [
     false,
     false, true, false, true, false, false,
     false, true, false, true, false, false
 ];
 
 /// Sliding no diagonal pieces
-pub const SLIDING_NO_DIAG_PIECES: [bool; PIECES_NUM] = [
+const SLIDING_NO_DIAG_PIECES: [bool; PIECES_NUM] = [
     false,
     false, true, true, false, false, false,
     false, true, true, false, false, false
 ];
+
+const COLOR_PIECES: [Color; PIECES_NUM] = [
+    Color::None,
+    Color::White, Color::White, Color::White,
+    Color::White, Color::White, Color::White,
+
+    Color::Black, Color::Black, Color::Black,
+    Color::Black, Color::Black, Color::Black,
+];
+
 
 impl Piece {
     /// Returns whether or not the piece is sliding
@@ -59,5 +133,11 @@ impl Piece {
     #[inline]
     pub fn is_sliding_no_diag(self) -> bool {
         SLIDING_NO_DIAG_PIECES[self as usize]
+    }
+
+    /// Returns the color
+    #[inline]
+    pub fn color(self) -> Color {
+        COLOR_PIECES[self as usize]
     }
 }
