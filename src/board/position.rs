8d2b8bdef69086fe2d3d0ba7f35f674e0be4ad48
César Belley <cesar.belley@lse.epita.fr>
2022-04-24 11:08:52 +0200

    Add position module
    Signed-off-by: César Belley <cesar.belley@lse.epita.fr>

---
+++ src/board/position.rs
@@ -0,0 +1,58 @@
+/// File representation of the board columns
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[repr(u8)]
+pub enum File {
+    A = 0,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    None,
+}
+
+/// Rank representation of the board rows
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Rank {
+    One = 0,
+    Tow,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    None,
+}
+
+/// Position representation on the board
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct Position {
+    /// File coordinate
+    file: File,
+    /// rank coordinate
+    rank: Rank,
+}
+
+impl Position {
+    /// Create a new `Position` instance
+    #[inline]
+    pub const fn new(file: File, rank: Rank) -> Self {
+        Self { file, rank }
+    }
+
+    /// Returns the file position
+    #[inline]
+    pub fn file(&self) -> File {
+        self.file
+    }
+
+    /// Returns the rank position
+    #[inline]
+    pub fn rank(&self) -> Rank {
+        self.rank
+    }
+}
